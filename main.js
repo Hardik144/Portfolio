397d859ff14bcce513ee1da35e3542cab07ffb08
Hardik144 <patidarhardik81@gmail.com>
2025-04-13 02:13:05 +0530

    complete initial version of website

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
 function openModalProject() {
   document.getElementById("modal").style.display = "flex";
-  document.getElementById("card_project").style.display = "flex";
+  document.getElementById("card_project").style.display = "block";
   document.getElementById("card_education").style.display = "none";
   document.getElementById("card_about").style.display = "none";
   document.getElementById("card_contact").style.display = "none";
 }
 function openModalEducation() {
     document.getElementById("modal").style.display = "flex";
-    document.getElementById("card_education").style.display = "flex";
+    document.getElementById("card_education").style.display = "block";
     document.getElementById("card_project").style.display = "none";
     document.getElementById("card_about").style.display = "none";
     document.getElementById("card_contact").style.display = "none";
 }
 function openModalAbout() {
     document.getElementById("modal").style.display = "flex";
-    document.getElementById("card_contact").style.display = "flex";
+    document.getElementById("card_contact").style.display = "none";
     document.getElementById("card_education").style.display = "none";
     document.getElementById("card_project").style.display = "none";
-    document.getElementById("card_about").style.display = "none";
+    document.getElementById("card_about").style.display = "block";
 }
 function openModalContact() {
     document.getElementById("modal").style.display = "flex";
-    document.getElementById("card_contact").style.display = "flex";
+    document.getElementById("card_contact").style.display = "block";
     document.getElementById("card_education").style.display = "none";
     document.getElementById("card_project").style.display = "none";
     document.getElementById("card_about").style.display = "none";
